4b27afe37d7cbf587d7d4f5993c04b0278fdae60
leoca42 <leo.carlin@outlook.com>
2025-12-16 13:13:29 -0800

    hehe

--- BasicNumberTheory.lean
+++
@@ -1,4 +0,0 @@
--- This module serves as the root of the `BasicNumberTheory` library.
--- Import modules here that should be built as part of the library.
-import BasicNumberTheory.Basic
-import BasicNumberTheory.Definitions
--- Main.lean
+++
@@ -1,6 +0,0 @@
-import BasicNumberTheory
-
-def main : IO Unit :=
-  IO.println "Hello, World!"
-
--- simply checks if the propositions are well-formed
