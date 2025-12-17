ce160abfdf227f965de1620da3080baad8087b0e
leoca42 <leo.carlin@outlook.com>
2025-12-17 08:36:46 -0800

    FIRST PROOF!!!!! even squared is even

--- basicnumbertheory/Main.lean
+++ basicnumbertheory/Main.lean
@@ -1,6 +1,31 @@
 import Basicnumbertheory
 
-import Mathlib.Algebra.Ring.Parity
+/-
+Syntax:
+- def defines a new term (constant, function, or type)
+EvenO (n : Nat) : Prop declares EvenO as a property of natural numbers
+- ∃ k : Nat, n = 2 * k states that there exists a natural number k
+- n = 2 * k means n is equal to twice k, which defines n as even
+-/
+def EvenO (n : Nat) : Prop := ∃ k : Nat, n = 2 * k
 
-#check Even 10
-#check Odd 11
+/-
+Syntax:
+- theorem declares a new theorem named even_squared_is_even
+- (n : Nat) introduces a natural number n as a variable
+- (h : EvenO n) introduces a hypothesis h that n is even
+- EvenO (n * n) states the conclusion that n squared is also even
+- by begins the proof
+-/
+-- theorem name input hypothesis conclusion proof
+theorem even_squared_is_even (n : Nat) (h : EvenO n) : EvenO (n * n) := by
+    --destructure the hypothesis h to extract k such that n = 2 * k
+    rcases h with ⟨k, hk⟩
+    -- refine the goal to show there exists a natural number (k * (2 * k)) such that n * n = 2 * (k * (2 * k))
+    -- _? is a placeholder
+    refine ⟨k * (2 * k), ?_⟩
+    -- After rewriting n via hk, (2*k)*(2*k) = 2*(k*(2*k)) by associativity
+    -- simp is a tactic that simplifies expressions using known equalities and properties
+    simp [hk, Nat.mul_assoc]
+
+    -- So, we're done!
